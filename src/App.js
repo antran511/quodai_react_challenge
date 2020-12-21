1c18c1e666bce99acbed874bc18e24cfffe77a6e
antran511 <kienan.tran511@gmail.com>
2020-12-21 10:35:52 +0700

    Add required dependencies and set up redux in application

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
 function App() {
   return (
-    <div>React challenge
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Typography variant="h3" gutterBottom>
+          Github issues
+        </Typography>
+      </Container>
+    </React.Fragment>
   );
 }
 
---
+++ src/reducers/issue.js
@@ -0,0 +1,9 @@
+const initialState = {
+    list: [],
+}
+
+const issueReducer = (state = initialState, action) => {
+    return state;
+}
+
+export default issueReducer;
